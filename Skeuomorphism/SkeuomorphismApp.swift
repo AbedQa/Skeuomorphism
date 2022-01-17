f65783dd50d47c3b543de2c19adb98fcfd9cd8d3
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-17 11:08:36 +0200

    Initial Commit

---
+++ Skeuomorphism/SkeuomorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeuomorphismApp.swift
+//  Skeuomorphism
+//
+//  Created by AbdelrahmanQasim on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SkeuomorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
